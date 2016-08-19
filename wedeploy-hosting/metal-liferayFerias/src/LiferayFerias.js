5d9c9b371762b299b5c915faa133ea51afc1b194
Fernando Souza <fernando.souza@liferay.com>
2016-08-19 14:07:17 -0300

    Adds metal component

---
+++ wedeploy-hosting/metal-liferayFerias/src/LiferayFerias.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import templates from './LiferayFerias.soy.js';
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+
+class LiferayFerias extends Component {
+}
+Soy.register(LiferayFerias, templates);
+
+export default LiferayFerias;
